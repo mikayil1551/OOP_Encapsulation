9ff4b44c3e542b82c430052202119df27e7da008
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-04-26 18:35:39 +0300

    Add files via upload

---
+++ Encapsulation/Encapsulation/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encapsulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Urun u = new Urun();
+            u.Adi="Admin";
+            u.Fiyat = 10;
+            MessageBox.Show(u.KdvliFiyat.ToString());
+        }
+    }
+}
